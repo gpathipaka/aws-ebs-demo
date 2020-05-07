595dd13768291cbcc5ebef5371471a70cf3dec5a
GP <gangadharind@gmail.com>
2020-05-07 17:53:08 -0400

    AWS Elastic Beanstack Demo and port mapping

---
+++ src/main/java/com/aws/ebs/EbsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbsDemoApplication.class, args);
+	}
+
+}
